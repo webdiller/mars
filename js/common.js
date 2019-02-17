91c74fd60c0c85de4a25afb5a3b377cb77dcfd7a
Eugene <eugenefromrus@gmail.com>
2019-02-17 23:23:50 +1000

    first commit

---
+++ js/common.js
@@ -0,0 +1,62 @@
+// $(document).ready(function () {
+//     // menu toggle BEGIN
+//      $(".menu").on("click", function () { 
+//          $(this).toggleClass("active");
+//          $(".overlay").toggleClass("menu-open");
+//       });
+//      $(".overlay ul li a").on("click", function () { 
+//         $(".menu").removeClass("active");
+//         $(".overlay").removeClass("menu-open");
+//       });
+//     // menu toggle END
+
+//     // carousel
+//     $("#carousel1").owlCarousel({
+//         loop              : true,
+//         autoplay          : false,
+//         nav               : false,
+//         autoplayTimeout   : 3000,
+//         autoplayHoverPause: false,
+//         smartSpeed        : 500,
+//         center            : true,
+//         items             : 1,
+//         dots              : true,
+//     });
+//     // carousel
+// });
+
+$(document).ready(function () {
+    var preloared = document.getElementById("preloader");
+    setTimeout(function () {
+        if (!preloared.classList.contains('done')) {
+            preloared.classList.add('done');
+        }
+    }, 1000);
+
+
+    // menu toggle BEGIN
+    $(".menu").on("click", function () {
+        $(this).toggleClass("active");
+        $(".overlay").toggleClass("menu-open");
+    });
+    $(".overlay ul li a").on("click", function () {
+        $(".menu").removeClass("active");
+        $(".overlay").removeClass("menu-open");
+    });
+    // menu toggle END
+
+    // carousel
+    $("#carousel1").owlCarousel({
+        loop: true,
+        autoplay: true,
+        nav: false,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: false,
+        smartSpeed: 500,
+        center: true,
+        items: 1,
+        dots: true,
+    });
+    //     // carousel
+
+});
